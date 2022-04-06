5fef5496d329c52ed8e9f7c977cdfb9cf15d276a
bserna13 <101461524+bserna13@users.noreply.github.com>
2022-04-05 22:43:38 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -4,19 +4,20 @@ class Grupo:
 
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
     def __str__(self):
+        
         return "Grupo de estudiantes: "+self._grupo
 
     def listadoAsignaturas(self, **kwargs):
         if self._asignaturas is None:
             lista = []
             for x in kwargs.values():
-                self._asignaturas.append(Asignatura(x))
+                lista.append(Asignatura(x))
                 self._asignaturas=lista
         else:
             for x in kwargs.values():
